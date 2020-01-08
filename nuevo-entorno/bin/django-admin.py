1a11df9e5aec7d4064639c405d969ba8f25cc054
tomas <tomascl1998@gmail.com>
2020-01-08 18:09:59 +0100

    Moviendo entorno virtual fuera del repositorio

--- nuevo-entorno/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/tomas/Escritorio/Exa1/EGC-ene-20T-tomcablop/nuevo-entorno/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
