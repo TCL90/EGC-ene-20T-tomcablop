a80abb5d1ca489f5b075b6a4e13e77265d54d718
tomas <tomascl1998@gmail.com>
2020-01-08 18:05:06 +0100

    Apicando cambios para que decide se ejecute correctamente en local desde un entorno virtual

---
+++ nuevo-entorno/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/tomas/Escritorio/Exa1/EGC-ene-20T-tomcablop/nuevo-entorno/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
